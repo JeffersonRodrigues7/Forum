7c2110545f84d38ef1e7a0bd67afa4d247bcd18b
JeffersonRodrigues7 <jefferson_rodrigues7@hotmail.com>
2022-04-21 23:35:22 -0300

    Pequenas alterações

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from "./views/components/Navbar/NavbarComp";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
     <React.StrictMode>
         <NavbarComp />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
